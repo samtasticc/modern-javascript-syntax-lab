f783475e1d242e365b8b0219ef1afac8a536e0ca
samtasticc <samantha.henesy@gmail.com>
2024-10-19 11:24:04 -0400

    exercise 4 apply spread operator on arrays

--- app.js
+++ app.js
@@ -17,36 +17,60 @@ console.log(nums2)
 
 // Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
 
-const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+// const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
 
 // Your code here
 
-const [bestTopping, worstTopping] = pizzaToppings
+// const [bestTopping, worstTopping] = pizzaToppings
 
-console.log(bestTopping)
-console.log(worstTopping)
+// console.log(bestTopping)
+// console.log(worstTopping)
 
 // ==== Exercise 3 === //
 
 // Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
+
+// const car = {
+//     make: 'Audi',
+//     model: 'q5',
+//   };
+  
+// Your code here
+
+// const {make, model} = car
+
+// console.log(make)
+// console.log(model)
+
+// ==== Exercise 4 === //
+
+// Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+
+const controversialPizzaToppings = [...pizzaToppings]
+
+console.log(controversialPizzaToppings)
+
+// ==== Exercise 5 === //
+
+// Duplicate the following object and spread its values into a new variable `myCar`.
 
 const car = {
     make: 'Audi',
     model: 'q5',
   };
   
-  // Your code here
+// Change the `model` property of `myCar` to 'q7'. Log both objects.
 
-const {make, model} = car
+// Your code here
 
-console.log(make)
-console.log(model)
 
-  // ==== Exercise 4 === //
-  // ==== Exercise 5 === //
-  // ==== Exercise 6 === //
-  // ==== Exercise 7 === //
-  // ==== Exercise 8 === //
-  // ==== Exercise 9 === //
-  // ==== Exercise 10 === //
-  // ==== Exercise 11 === //
+// ==== Exercise 6 === //
+// ==== Exercise 7 === //
+// ==== Exercise 8 === //
+// ==== Exercise 9 === //
+// ==== Exercise 10 === //
+// ==== Exercise 11 === //
